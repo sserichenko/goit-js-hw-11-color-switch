a0218e5d6ae0f49c05971a8050eb851fae56853a
sserichenko <sserichenko@gmail.com>
2019-10-29 08:55:15 +0200

    done color switch hw_11

---
+++ js/index.js
@@ -0,0 +1,38 @@
+const body = document.querySelector('body');
+const startButton  = document.querySelector('[data-action="start"]');
+const stopButton  = document.querySelector('[data-action="stop"]');
+let customTimerId = null;
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const randomIntegerFromInterval = (min, max) => {
+  return  Math.floor(Math.random() * (max - min + 1) + min);
+};
+let min = 0;
+let max = colors.length -1;
+
+
+const changerColor = {
+  isActive: false,
+  start(){
+    if(this.isActive){
+      return;
+    }
+    this.isActive = true;  
+    customTimerId = setInterval(() => {
+    body.style.backgroundColor = colors[randomIntegerFromInterval(min, max)];
+  }, 1000);
+  },
+  stop(){
+    clearInterval(customTimerId);
+  }
+};
+
+startButton.addEventListener('click', changerColor.start.bind(changerColor));
+stopButton.addEventListener('click', changerColor.stop.bind(changerColor));
